8281130cf91e9a3de2f37b2edd6d37397f0077da
NW230801R\n-kimura <n-kimura@clinks.jp>
2023-10-02 10:26:52 +0900

    init

---
+++ hooks/validation.ts
@@ -0,0 +1,12 @@
+const validation = {
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    })
+  },
+}
+
+export { validation }
