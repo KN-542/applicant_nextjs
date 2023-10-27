1dcff20ba79a2f17d21431c847e8a36702e11551
NW230801R\n-kimura <n-kimura@clinks.jp>
2023-10-27 17:09:46 +0900

    Toast

--- pages/management/admin/setting/index.tsx
+++ pages/management/admin/setting/index.tsx
@@ -1,7 +1,7 @@
 import Content from '@/components/Content'
 import { mgChangeSetting } from '@/hooks/store'
 import store, { RootState } from '@/hooks/store/store'
-import { Contents } from '@/types/management'
+import { Color, Contents } from '@/types/management'
 import { deepPurple, indigo, red } from '@material-ui/core/colors'
 import { Box, Button, Typography } from '@mui/material'
 import { useTranslations } from 'next-intl'
@@ -10,6 +10,20 @@ import { useRouter } from 'next/router'
 import { RouterPath } from '@/enum/router'
 import { map } from 'lodash'
 import NextHead from '@/components/Header'
+import {
+  amber,
+  blue,
+  brown,
+  common,
+  cyan,
+  deepOrange,
+  green,
+  grey,
+  lime,
+  pink,
+  teal,
+  yellow,
+} from '@mui/material/colors'
 
 const Setting = () => {
   const router = useRouter()
@@ -17,18 +31,226 @@ const Setting = () => {
 
   const setting = useSelector((state: RootState) => state.management.setting)
 
-  const colors = [
+  const colorSet: Color[] = [
     {
-      label: 'indigo',
-      values: [indigo[300], indigo[500], indigo[800]],
+      color: indigo[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
     },
     {
-      label: 'purple',
-      values: [deepPurple[300], deepPurple[500], deepPurple[800]],
+      color: indigo[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
     },
     {
-      label: 'red',
-      values: [red[300], red[500], red[800]],
+      color: indigo[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: blue[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: blue[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: blue[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: deepPurple[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: deepPurple[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: deepPurple[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: common.black,
+      toastSuccessColor: green[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: red[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: red[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: red[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: deepOrange[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: deepOrange[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: deepOrange[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: deepOrange[900],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: pink[300],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: pink[500],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: pink[800],
+      toastSuccessColor: green[500],
+      toastErrorColor: common.black,
+    },
+    {
+      color: green[300],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: green[500],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: green[800],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: cyan[300],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: cyan[500],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: cyan[800],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: teal[300],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: teal[500],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: teal[800],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: teal[900],
+      toastSuccessColor: blue[500],
+      toastErrorColor: red[500],
+    },
+    {
+      color: lime[300],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: lime[500],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: lime[800],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: amber[300],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: amber[500],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: amber[800],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: brown[300],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: brown[500],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: brown[800],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: brown[900],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: grey[300],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: grey[500],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: grey[800],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
+    },
+    {
+      color: grey[900],
+      toastSuccessColor: green[800],
+      toastErrorColor: red[500],
     },
   ]
 
@@ -57,7 +279,7 @@ const Setting = () => {
           }}
         >
           <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
-            {map(colors, (colorGroup, index) => (
+            {map(colorSet, (obj, index) => (
               <Box
                 key={index}
                 sx={{
@@ -66,35 +288,28 @@ const Setting = () => {
                   flexWrap: 'nowrap',
                 }}
               >
-                {map(colorGroup.values, (colorValue, idx) => (
-                  <Button
-                    key={idx}
-                    variant="contained"
-                    sx={{
-                      width: 100,
-                      height: 100,
-                      backgroundColor: colorValue,
-                      color: '#fff',
-                      textTransform: 'none',
-                      '&:hover': {
-                        backgroundColor: colorValue,
-                      },
-                    }}
-                    onClick={(e) => {
-                      e.preventDefault()
+                <Button
+                  variant="contained"
+                  sx={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: obj.color,
+                    color: '#fff',
+                    textTransform: 'none',
+                    '&:hover': {
+                      backgroundColor: obj.color,
+                    },
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault()
 
-                      store.dispatch(
-                        mgChangeSetting({
-                          color: colorValue,
-                        }),
-                      )
+                    store.dispatch(mgChangeSetting(obj))
 
-                      router.push(RouterPath.ManagementSetting)
-                    }}
-                  >
-                    <Typography variant="body2">{colorValue}</Typography>
-                  </Button>
-                ))}
+                    router.push(RouterPath.ManagementSetting)
+                  }}
+                >
+                  <Typography variant="body2">{obj.color}</Typography>
+                </Button>
               </Box>
             ))}
           </Box>
